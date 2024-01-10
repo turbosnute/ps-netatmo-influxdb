7a3f3fa8525e0d0d04019f7dbb10c05aa9e7b0ae
Øyvind Andreas Nilsen <on@ntnu.no>
2024-01-10 15:05:15 +0100

    random state

--- web/index.php
+++ web/index.php
@@ -3,22 +3,22 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>ps-netatmo-influxdb</title>
+        <title>PS Netatmo InfluxDB</title>
         <link rel="stylesheet" href="simple.min.css">
     </head>
 
     <body>
         <header>
-            <h1>PS Netatmo Influxdb</h1>
+            <h1>PS Netatmo InfluxDB</h1>
         </header>
 
         <main>
-            <p><?php echo 'My username is ' .$_ENV["USER"] . '!'; ?></p>
             <?php
+                $state = rand(100000000,900000000);
                 $client_id = $_ENV['client_id'];
                 $redirect_uri = $redirect_uri =  $_SERVER['REQUEST_SCHEME']."://".$_SERVER['HTTP_HOST']."/auth.php";
                 $scope = 'read_station';
-                $url = "https://api.netatmo.com/oauth2/authorize?client_id=".$client_id."&redirect_uri=".$redirect_uri."&scope=".$scope."&state=jieoadjsoadoijeeer134";
+                $url = "https://api.netatmo.com/oauth2/authorize?client_id=".$client_id."&redirect_uri=".$redirect_uri."&scope=".$scope."&state=$state";
                 //echo $url;
             ?>
             <a class="button" href="<?php echo $url; ?>">Authenticate</a>
