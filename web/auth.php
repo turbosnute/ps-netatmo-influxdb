accfd4e46c79cba6e403b68fcf792925bdfff6bb
Øyvind Andreas Nilsen <on@ntnu.no>
2024-01-10 14:25:36 +0100

    auth stuff

--- web/auth.php
+++ web/auth.php
@@ -1,4 +1,7 @@
 <?php
+    $uri = 'https://api.netatmo.com/oauth2/token';
+    $grant_type = 'authorization_code';
+    $scope = 'read_station';
 
     if (isset($_GET['code'])) {
         $code = $_GET['code'];  
@@ -11,10 +14,6 @@
     } else {
         // hmmmm?
     }
-    
-    $uri = 'https://api.netatmo.com/oauth2/token';
-
-    $grant_type = 'authorization_code';
 
     if (isset($_ENV['client_id'])) {
         $client_id = $_ENV['client_id'];
@@ -30,8 +29,24 @@
 
     $code = $code;
     $redirect_uri = '';
-    $scope = 'read_station';
 
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $uri);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+        'code' => $code,
+        'client_id' => $client_id,
+        'client_secret' => $client_secret,
+        'redirect_uri' => $redirect_uri,
+        'grant_type' => 'authorization_code'
+    ));
+        
+    $data = curl_exec($ch);
+    
+    var_dump($data);
+    
     /*
 HTTP/1.1 200 OK
     Content-Type: application/json;charset=UTF-8
